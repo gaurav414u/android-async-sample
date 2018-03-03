3307f6f2244777fa2be0285a46c10217cb739cb5
Gaurav Bhola <gaurav414u@gmail.com>
2018-03-03 11:24:55 +0530

    Using the local dev server url in the app

--- android-async-sample/app/src/main/java/com/gauravbhola/asyncsample/util/Const.java
+++ android-async-sample/app/src/main/java/com/gauravbhola/asyncsample/util/Const.java
@@ -2,5 +2,5 @@ package com.gauravbhola.asyncsample.util;
 
 
 public class Const {
-    public static final String API_BASE_URL = "https://contactsservice.herokuapp.com";
+    public static final String API_BASE_URL = "http://192.168.43.99:5000";
 }
